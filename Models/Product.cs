e54d3891c7df5cc53dd28bc3336dcb97a55067b0
DanielGomez <killergomez777@gmail.com>
2023-11-19 17:33:04 -0500

    product

---
+++ Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SupermarkerWEB.Models
+{
+    public class Product
+    {
+        //[Key] -> Anotacion si la propiedad no se llama id, ejemplo ProductId
+        public int Id { get; set; } //Sera la llave Primaria
+        public string Name { get; set; }
+        [Column(TypeName = "decimal(6,2)")]
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public int? CategoryId { get; set; } //Sera la llave primaria
+        public Category? Category { get; set; } // Propiedad de Navegación
+    }
+}
