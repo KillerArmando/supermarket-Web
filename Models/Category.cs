60f438a8e0bdd3bd0f0b216a5d2d3e5ef58f5c20
DanielGomez <killergomez777@gmail.com>
2023-11-19 17:26:05 -0500

    category

---
+++ Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace SupermarkerWEB.Models
+{
+    public class Category
+    {
+        public int Id { get; set; } // Sera la llave Primaria
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public ICollection<Product>? Products { get; set; } // Propiedad de Navegacion
+    }
+}
